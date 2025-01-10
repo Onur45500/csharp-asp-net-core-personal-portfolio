f905b0314335ade0652930ff0e6cfeb3ac2cbbdc
Onur AKMESE <onur.akmese@viacesi.fr>
2025-01-10 22:52:59 +0100

    Added the experiences and a hero

---
+++ csharp-asp-net-core-personal-portfolio/Models/Experience.cs
@@ -0,0 +1,10 @@
+namespace csharp_asp_net_core_personal_portfolio.Models
+{
+    public class Experience
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public string Duration { get; set; }
+        public List<string> Responsibilities { get; set; }
+    }
+}
